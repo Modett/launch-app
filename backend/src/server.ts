3d07a2bacb6838ec9124bbd976f169468042c4cf
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-01 00:34:36 +0530

    updates

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import routes from "./routes";
+import { IncomingMessage, ServerResponse } from "http";
 
 const server = Fastify({
   logger: true,
@@ -13,7 +14,10 @@ server.register(cors, {
 server.register(routes);
 
 // For Vercel serverless - export the handler
-export default async function handler(req: any, res: any) {
+export default async function handler(
+  req: IncomingMessage,
+  res: ServerResponse,
+) {
   await server.ready();
   server.server.emit("request", req, res);
 }
